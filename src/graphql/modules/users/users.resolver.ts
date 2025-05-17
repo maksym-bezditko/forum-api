b373481b85e93c36f9dc8e67bce91b69349c86b3
Maksym Bezditko <130133118+maksym-bezditko@users.noreply.github.com>
2025-05-17 11:39:50 +0300

    Update users resolver to include login and register mutations

--- src/graphql/modules/users/users.resolver.ts
+++ src/graphql/modules/users/users.resolver.ts
@@ -1,6 +1,8 @@
 import { Context } from '../../models';
 import { Forum } from '../forums/models/forum';
 import { JoinForumInput } from './models/join-forum-input';
+import { LoginInput, RegisterInput } from './models/auth-input';
+import { AuthResponse } from './models/auth-response';
 import { UsersService } from './users.service';
 
 export const usersResolver = {
@@ -11,6 +13,20 @@ export const usersResolver = {
       context: Context,
     ): Promise<Forum> {
       return UsersService.joinForum(args, context.loggedUser);
+    },
+    
+    async login(
+      _: unknown,
+      { email, password }: { email: string; password: string },
+    ): Promise<AuthResponse> {
+      return UsersService.login({ email, password });
+    },
+
+    async register(
+      _: unknown,
+      { input }: { input: RegisterInput },
+    ): Promise<AuthResponse> {
+      return UsersService.register(input);
     },
   },
 };
